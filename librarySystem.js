d2c3ce5e0018fe4d28272912224f633e1f882230
Lokesh Jadhav <lokesh.jadhav10@gmail.com>
2017-11-01 00:49:15 +0100

    Initial setup and creating librarySystem using window variable approach

---
+++ librarySystem.js
@@ -0,0 +1,20 @@
+(function(){
+
+  var breads = {
+    wheat: 'The healthy option',
+    white: 'The unhealthy option'
+  };
+
+  var fillings = {
+    turkey: 'For boring sandwiches',
+    cheese: 'For the vegetarians'
+  };
+
+  var sandwichLibrary = {
+    breads: breads,
+    fillings: fillings
+  }
+
+  window.sandwichLibrary = sandwichLibrary;
+
+})();
