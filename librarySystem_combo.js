739e5ddba9cc50eadc12c2d4dc67ffdfd41bdedf
Lokesh Jadhav <lokesh.jadhav10@gmail.com>
2017-11-01 18:52:51 +0100

    Add the noConflict functionality to reset to our old library

--- librarySystem_combo.js
+++ librarySystem_combo.js
@@ -1,3 +1,7 @@
+//window.sandwichJS has an original value.
+window.sandwichLibrary = 'Library with books about sandwiches.'
+
+//sandwichJS loads.
 (function(){
 
   var breads = {
@@ -22,14 +26,34 @@
   //   return sandwichLibrary;
   // })
   // But the above method is not dynamic.
-  
+
   //if librarySystem function exists then use that or else use window.sandwichLibrary variable
   if(typeof librarySystem !== 'undefined'){
     librarySystem('sandwichLibrary', function(){
       return sandwichLibrary;
     });
   }else{
+
+    var oldSandwichLibrary = window.sandwichLibrary;
+
+    sandwichLibrary.noConflict = function(){
+      window.sandwichLibrary = oldSandwichLibrary;
+      return sandwichLibrary;
+    };
+
     window.sandwichLibrary = sandwichLibrary;
   }
 
 })();
+
+// This will reset window.sandwichLibrary to the original value which is the string.
+// Then how will we use our own sandwichLibrary.So the solution is noConflict() will return our sandwichLibrary.
+var sandwichJS = sandwichLibrary.noConflict();
+
+// You want to print window.sandwichLibrary i.e. you want the string library with books about sandwiches
+// But you won't be able to because in the else loop we override window.sandwichLibrary with our library.
+// To avoid this we run a noConflict()
+console.log(sandwichLibrary);
+
+// We can still use sandwichJS.
+console.log(sandwichJS.breads.white);
